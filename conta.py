ec382911cc6cdc2b7a727324cb1678e8e19d7e5a
Jose Ivo <jose.lima@dcx.ufpb.br>
2021-04-22 09:49:16 -0300

    add more 3 test, added a correction in conta.py and added her exceptions

--- conta.py
+++ conta.py
@@ -2,22 +2,27 @@ class Conta:
 
     # self é a referência que sabe encontrar o objeto construído em memória.
     def __init__(self, numero, titular, saldo, limite):
-        print("Construindo objeto...{}".format(self))
         self.__numero = numero
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
         
     def extrato(self):
-        print("Saldo {} do titular {}".format(self.__saldo, self.__titular))
+        return "O titular: {} possui um saldo de {}R$".format(self.__titular, self.__saldo)
         
     def deposita(self, valor):
         self.__saldo += valor
 
     def saca(self, valor):
+        if(self.get_saldo() < valor):
+            raise RuntimeError("Valor de saldo insuficiente")
         self.__saldo -= valor
 
     def transfere(self, valor, destino):
+        if(self.get_saldo() < valor):
+            raise RuntimeError("Valor de saldo insuficiente")
+        if(destino == None or destino.get_limite_disponivel() < valor):
+            raise RuntimeError("Destinatario não existe ou não possui limite")
             self.saca(valor)
             destino.deposita(valor)
         
@@ -27,8 +32,13 @@ class Conta:
     def get_titular(self):
         return self.__titular
 
-    def get_limite(self):
+    def get_limite_total(self):
         return self.__limite
+    
+    def get_limite_disponivel(self):
+        #limite disponivel é o limite total menos oque que já tem
+        limite_disponivel_uso = self.get_limite_total() - self.get_saldo()
+        return limite_disponivel_uso
     
     def set_limite(self, limite):
         self.__limite = limite
--- test_conta.py
+++ test_conta.py
@@ -4,9 +4,27 @@ from conta import Conta
 
 class TestDatas(unittest.TestCase):
     def setUp(self):
-        #instância objeto
-        pass
+        self.conta = Conta(1, "josé ivo", 300, 1000)
+        self.conta_destino = Conta(2, "destinatario", 800, 2000)
     
+    def test_extract(self):
+        self.assertEqual(self.conta.extrato(),
+                         "O titular: josé ivo possui um saldo de 300R$")
+        
+    def test_checkSafeMoney(self):
+        self.conta.deposita(700)
+        self.assertEqual(self.conta.get_saldo(), 1000)
+        
+    #verificar se o valor é realmente retirado, se houver saldo suficiente
+    def test_checkWithdraw(self):
+        valorDeSaque = 300
+        self.conta.saca(valorDeSaque)
+        self.assertEqual(self.conta.get_saldo(),0)
     
+    def test_checkRecipient(self):
+        valorDeTransferencia = 300
+        self.conta.transfere(valorDeTransferencia, self.conta_destino)
+        
+    #Verificar que ao transderir o destino exista e se tem valor suficiente
 if __name__ == '__main__':
     unittest.main()
